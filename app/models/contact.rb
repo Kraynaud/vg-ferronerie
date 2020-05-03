19e131263c2e96f382a6b5d7a3c6617a5cb12e8f
kevin <kraynaud@gmail.com>
2020-05-03 17:23:19 +0200

    done

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  validates :prenom, presence: true
+  validates :nom, presence: true
+  validates :description, presence: true
+  validates :objet, presence: true
+  validates :email, presence: true
 end
